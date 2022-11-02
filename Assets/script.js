7b48fc4852387990846d1fcb9f5f7b35089ab70f
CDoolhoff <109476450+christopherdoolhoff@users.noreply.github.com>
2022-11-01 22:29:45 -0400

    added editable proporty to timeblocks

--- Assets/script.js
+++ Assets/script.js
@@ -2,6 +2,7 @@
 var today = moment().format("ddd MMM Do, YYYY");
 var time = moment().format("k");
 var eightAM = document.getElementById("8AM");
+var eightAMp = document.getElementById("8AMp");
 var nineAM = document.getElementById("9AM");
 var tenAM = document.getElementById("10AM");
 var elevenAM = document.getElementById("11AM");
@@ -14,82 +15,83 @@ var fivePM = document.getElementById("5PM");
 // display current date at top of application.
 $("#today").text(today);
 // change css based on if time slot is past, preset, or future.
-if (time>8){
-    eightAM.classList.add("past");
-} else if (time==8){
-    eightAM.classList.add("present");
+if (time > 8) {
+  eightAM.classList.add("past");
+} else if (time == 8) {
+  eightAM.classList.add("present");
 } else {
-    eightAM.classList.add("future");
+  eightAM.classList.add("future");
 }
 
-if (time>9){
-    nineAM.classList.add("past");
-} else if (time==9){
-    nineAM.classList.add("present");
+if (time > 9) {
+  nineAM.classList.add("past");
+} else if (time == 9) {
+  nineAM.classList.add("present");
 } else {
-    nineAM.classList.add("future");
+  nineAM.classList.add("future");
 }
 
-if (time>10){
-    tenAM.classList.add("past");
-} else if (time==10){
-    tenAM.classList.add("present");
+if (time > 10) {
+  tenAM.classList.add("past");
+} else if (time == 10) {
+  tenAM.classList.add("present");
 } else {
-    tenAM.classList.add("future");
+  tenAM.classList.add("future");
 }
 
-if (time>11){
-    elevenAM.classList.add("past");
-} else if (time==11){
-    elevenAM.classList.add("present");
+if (time > 11) {
+  elevenAM.classList.add("past");
+} else if (time == 11) {
+  elevenAM.classList.add("present");
 } else {
-    elevenAM.classList.add("future");
+  elevenAM.classList.add("future");
 }
 
-if (time>12){
-    noon.classList.add("past");
-} else if (time==12){
-    noon.classList.add("present");
+if (time > 12) {
+  noon.classList.add("past");
+} else if (time == 12) {
+  noon.classList.add("present");
 } else {
-    noon.classList.add("future");
+  noon.classList.add("future");
 }
 
-if (time>13){
-    onePM.classList.add("past");
-} else if (time==13){
-    onePM.classList.add("present");
+if (time > 13) {
+  onePM.classList.add("past");
+} else if (time == 13) {
+  onePM.classList.add("present");
 } else {
-    onePM.classList.add("future");
+  onePM.classList.add("future");
 }
 
-if (time>14){
-    twoPM.classList.add("past");
-} else if (time==14){
-    twoPM.classList.add("present");
+if (time > 14) {
+  twoPM.classList.add("past");
+} else if (time == 14) {
+  twoPM.classList.add("present");
 } else {
-    twoPM.classList.add("future");
+  twoPM.classList.add("future");
 }
 
-if (time>15){
-    threePM.classList.add("past");
-} else if (time==15){
-    threePM.classList.add("present");
+if (time > 15) {
+  threePM.classList.add("past");
+} else if (time == 15) {
+  threePM.classList.add("present");
 } else {
-    threePM.classList.add("future");
+  threePM.classList.add("future");
 }
 
-if (time>16){
-    fourPM.classList.add("past");
-} else if (time==16){
-    fourPM.classList.add("present");
+if (time > 16) {
+  fourPM.classList.add("past");
+} else if (time == 16) {
+  fourPM.classList.add("present");
 } else {
-    fourPM.classList.add("future");
+  fourPM.classList.add("future");
 }
 
-if (time>17){
-    fivePM.classList.add("past");
-} else if (time==17){
-    fivePM.classList.add("present");
+if (time > 17) {
+  fivePM.classList.add("past");
+} else if (time == 17) {
+  fivePM.classList.add("present");
 } else {
-    fivePM.classList.add("future");
+  fivePM.classList.add("future");
 }
+
